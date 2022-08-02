fd23913abaa312331a39731f57ae16279f11bc2e
AntoshaLev <www.Garu33@mail.ru>
2022-08-03 00:26:59 +0300

    Lesson_1

--- backend/TODOnotes/serializers.py
+++ backend/TODOnotes/serializers.py
@@ -1,15 +1,8 @@
 from rest_framework.serializers import ModelSerializer
 from .models import User
-# from .models import Admin
 
 
 class UserModelSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-
-
-# class AdminModelSerializer(ModelSerializer):
-#     class Meta:
-#         model = Admin
-#         fields = '__all__'
