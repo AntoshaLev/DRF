bf2db45269541b0b31ac8d2e6c987ed970d148ef
AntoshaLev <90862359+AntoshaLev@users.noreply.github.com>
2022-08-10 01:54:57 +0300

    Lesson_3

--- backend/TODOnotes/views.py
+++ backend/TODOnotes/views.py
@@ -1,6 +1,12 @@
+import io
+
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.viewsets import ModelViewSet
-from .models import User
-from .serializers import UserModelSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import User, Project, ToDo
+from .serializers import UserModelSerializer, UserSerializer, ProjectModelSerializer, ToDoModelSerializer
 
 
 class UserModelViewSet(ModelViewSet):
@@ -8,4 +14,45 @@ class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
 
 
+class ProjectModelViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
 
+
+class ToDoModelViewSet(ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoModelSerializer
+
+
+def user_get(request, pk=None):
+    if pk is not None:
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user)
+    else:
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+    #    print(serializer.data)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data)
+
+
+@csrf_exempt
+def user_post(request, pk=None):
+    json_data = JSONParser().parse(io.BytesIO(request.body))
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=json_data)
+    elif request.method == 'PUT':
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, data=json_data)
+    elif request.method == 'PATCH':
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, data=json_data, partial=True)
+
+    if serializer.is_valid():
+        user = serializer.save()
+        serializer = UserSerializer(user)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data)
+
+    return HttpResponseBadRequest(JSONRenderer().render(serializer.errors))
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from TODOnotes.views import UserModelViewSet
+from TODOnotes.views import UserModelViewSet, ProjectModelViewSet, ToDoModelViewSet, user_post, user_get
+
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('ToDo', ToDoModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('user_post', user_post),
+    path('user_post/<int:pk>', user_post),
+    path('user_get', user_get),
+    path('user_get/<int:pk>', user_get),
 ]
