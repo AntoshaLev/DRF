0768cb64a37311603032aeb0b494e31be233c5ac
AntoshaLev <90862359+AntoshaLev@users.noreply.github.com>
2022-08-30 18:36:40 +0300

    Lesson 7 (#8)
    * Lesson_6
    
    * Lesson_7

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from TODOnotes.views import UserModelViewSet, ProjectModelViewSet, ToDoModelViewSet, user_post, user_get, UserApiView
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
@@ -11,6 +12,8 @@ router.register('todos', ToDoModelViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth-token/', obtain_auth_token),
     path('user_post', user_post),
     path('user_post/<int:pk>', user_post),
     path('user_get', user_get),
