cb1f7af0c1126763a1e0391be246d58341ce777c
AntoshaLev <90862359+AntoshaLev@users.noreply.github.com>
2022-09-02 13:40:51 +0300

    Lesson_8 (#9)

--- backend/TODOnotes/tests.py
+++ backend/TODOnotes/tests.py
@@ -1,3 +1,79 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APIClient, APITestCase, \
+    force_authenticate
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import ToDo
+from .views import UserModelViewSet, ToDoModelViewSet, ProjectModelViewSet
+from mixer.backend.django import mixer
 
-# Create your tests here.
+
+class TestUserApi(TestCase):
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        admin = User.objects.create_superuser(
+            username='admin',
+            email='test@mail.com',
+            password='qwerty'
+        )
+        request = factory.get('/api/users')
+        force_authenticate(request, admin)
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+    def test_get_list_with_data(self):
+        User.objects.create_user(
+            username='test',
+            email='some@mail.com',
+            password='123'
+        )
+        factory = APIRequestFactory()
+        request = factory.get('/api/users')
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(len(response.data), 1)
+
+    def test_get_list_client(self):
+        admin = User.objects.create_superuser(
+            'admin',
+            email='test@mail.com',
+            password='qwerty'
+        )
+        client = APIClient()
+        response = client.get('/api/users/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(len(response.data), 1)
+
+
+class TestUserClientApi(APITestCase):
+    def setUp(self) -> None:
+        self.admin = User.objects.create_superuser(
+            'admin',
+            email='test@mail.com',
+            password='qwerty'
+        )
+        self.user = User.objects.create_user(
+            username='test',
+            email='some@mail.com',
+            password='123'
+        )
+        self.todo = mixer.blend(
+            ToDo,
+            todo_text='Do something'
+        )
+
+    def test_get_list_client_test_case(self):
+        self.client.login(username='admin', password='qwerty')
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_get_list_403(self):
+        self.client.login(username='admin', password='qwerty')
+        self.client.logout()
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,12 +138,12 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS':
     #     'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 100,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        # http://login:password@127.0.0.1:8000/api/
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.SessionAuthentication',
+    #     # http://login:password@127.0.0.1:8000/api/
+    #     'rest_framework.authentication.BasicAuthentication',
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ]
 }
 
 # if DEBUG:
